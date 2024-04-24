d9d670ec4105a3cc4da7703169a595082c06a1dd
arkytechie <arkesh_sharma@hotmail.com>
2024-04-24 10:44:50 -0400

    new scripts added

---
+++ helloWorld.groovy
@@ -0,0 +1,12 @@
+class Emp {
+   int empID;
+   String empName;
+	
+   static void main(String[] args) {
+      Emp ep = new Emp();
+      ep.empID = 1000;
+      ep.empName = "Arkesh";   
+      Print 'Hello World!';  
+   } 
+}
+
